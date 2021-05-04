7c1996471c6726d9e8e7b91dcf9fd6034cb7e019
l_faithfull <hello@faithfulldesign.com>
2021-05-04 17:12:53 +0100

    Hover pseudo-class and top button added

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//get button 
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+//when user scrolls down 20px from top of doc, show button
+window.onscroll = function() {scrollFunc()};
+
+function scrollFunc() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+//scroll event listener
+window.addEventListener("scroll", scrollFunc);
+
+// When user clicks button, scroll to top of doc
+function topFunction() {
+    document.body.scrollTop = 0;    //for Sarfari 
+    document.documentElement.scrollTop = 0; //for Chrome, Firefox, IE, Opera
+}
---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* get button */
+topButton = document.getElementById("topBtn");
+
+/* when user scrolls down 20px from top of doc, show button */
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
